ccaab0cb52a184a09c2bbbc3700c142ecf06f253
Yasinchak Bohdan <yasinchakb@gmail.com>
2023-02-21 15:55:57 +0200

    logging

--- Examples/NSL.Node.RoomServerExample/Program.cs
+++ Examples/NSL.Node.RoomServerExample/Program.cs
@@ -6,6 +6,11 @@ namespace NSL.Node.BridgeTransportExample
     {
         public static void Main(string[] args)
         {
+            var x = new RoomConfigurationManager(null);
+            foreach (var r in x.GetAllValues())
+            {
+                Console.WriteLine($"{r.Path}  ::  {r.Value}");
+            }
             RoomServerStartupEntry.CreateDefault().RunEntry();
 
             Thread.Sleep(Timeout.InfiniteTimeSpan);
